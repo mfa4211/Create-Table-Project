ddb65a45103c3a76f0504a09bf41cfc29d817c64
mfa4211 <bangfadli4211@gmail.com>
2022-03-18 13:01:45 +0700

    Add project files.

---
+++ Create Table Project/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_Table_Project
+{
+    class Program
+    {
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=WINDOWS-CD7O5KF\\User;DATABASE=ProdiTI");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("Create table Mahasiswa_Coba(NIM char(12) not nu primary key," +
+                    "Nama varchar(50), Alamat varchar(255), Jenis_Kelamin char (1))", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Tabel sukses dibuat!");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oops, sepertinya ada yang salah. " + e);
+                Console.ReadKey();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        static void Main(string[] args)
+        {
+            new Program().CreateTable();
+        }
+    }
+}
